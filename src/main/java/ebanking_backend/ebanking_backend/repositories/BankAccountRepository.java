c92ae8b2dd324b58b6982304e7b3c14c2401d9c2
Harmal-Firdaous <127455706+Harmal-Firdaous@users.noreply.github.com>
2025-12-10 11:22:51 +0100

    Remove unused import in BankAccountRepository
    Removed unused import statement for List.

--- src/main/java/ebanking_backend/ebanking_backend/repositories/BankAccountRepository.java
+++ src/main/java/ebanking_backend/ebanking_backend/repositories/BankAccountRepository.java
@@ -3,7 +3,6 @@ package ebanking_backend.ebanking_backend.repositories;
 import ebanking_backend.ebanking_backend.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
    }
